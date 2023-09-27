3621a31ccd4f75e3e0c496c54c92c409cb61403e
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-09-27 18:41:14 +0700

    update server

--- src/libraries/axiosClient.js
+++ src/libraries/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
